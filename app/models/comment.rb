cd0ac6d5a5671517a48c0d7f5ad8292cef63294c
Colin Campbell <colindcamp@gmail.com>
2014-05-12 20:45:06 -0700

    maybe

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,16 +1,16 @@
 class Comment
   include Mongoid::Document
   include Mongoid::Timestamps
-  include Mongoid::Paperclip
+  
 
-  def self.after_commit(*args, &block)
-    args.each do |arg|
-    case arg[:on]
-      when :destroy
-        after_destroy &block
-      end
-    end
-  end
+  # def self.after_commit(*args, &block)
+  #   args.each do |arg|
+  #   case arg[:on]
+  #     when :destroy
+  #       after_destroy &block
+  #     end
+  #   end
+  # end
 
 
   field :name, type: String
@@ -18,13 +18,13 @@ class Comment
   field :time, type: String
   field :message, type: String
 
-  has_mongoid_attached_file :image,
-   :styles => {
-      :thumb => "100x100#",   # Centrally cropped
-      :small  => "150x150>",  # Only squish if it's larger than this
-      :elongate => "20x500"}
+  # has_mongoid_attached_file :image,
+  #  :styles => {
+  #     :thumb => "100x100#",   # Centrally cropped
+  #     :small  => "150x150>",  # Only squish if it's larger than this
+  #     :elongate => "20x500"}
 
 
-  validates_attachment_content_type :image,
-	 :content_type => /\Aimage\/.*\Z/
+  # validates_attachment_content_type :image,
+	 # :content_type => /\Aimage\/.*\Z/
 end
